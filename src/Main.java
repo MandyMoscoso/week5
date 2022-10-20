dd8964a86e1902d9afead0496b6c460cd093aa06
mdm177 <quyenmoscoso@gmail.com>
2022-10-20 15:00:29 -0500

    done problem #2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] listNum = {2, 1, 4, 3, 6, 5, 7, 10, 9};
-        int maxNum = 10;
-        MissingNum missingNum = new MissingNum();
-        System.out.println(missingNum.getMissing(listNum,maxNum));
+
+        //Missing num check
+//        int[] listNum = {2, 1, 4, 3, 6, 5, 7, 10, 9};
+//        int maxNum = 10;
+//        MissingNum missingNum = new MissingNum();
+//        System.out.println(missingNum.getMissing(listNum,maxNum));
+
+
+        String checkParens = "";
+        BalancedParentheses balancedParentheses = new BalancedParentheses();
+        System.out.println(balancedParentheses.parenCheck(checkParens));
 
     }
 }
